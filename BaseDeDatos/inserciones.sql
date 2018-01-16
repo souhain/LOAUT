4c3953e250d7e4c62532ca05599d40f37d5ddb67
Javier Armenta <javarmgar@gmail.com>
2018-01-15 22:34:23 -0600

    javie@DESKTOP-KDFKL47 MINGW64 ~/Desktop/Trabajo/repositorioOnline (master) $ git add *
    javie@DESKTOP-KDFKL47 MINGW64 ~/Desktop/Trabajo/repositorioOnline (master)
    $ git commit -m

---
+++ BaseDeDatos/inserciones.sql
@@ -0,0 +1,23 @@
+insert into cliente2 (name, address, email, phone) values
+( "Luis Vasquez Choreño",  "Estado de Mexico numero 2", "luis.chore@microservicios.com","5545178485"),
+( "javier armenta",  "Estado de Hidalgo numero 2", "javi@microservicios.com","5545733835"),
+( "luis hernandez",  "Estado de sinaloa numero 2", "luis@microservicios.com","5543473385"),
+( "edgar armenta",  "Estado de Hidalgo numero 2", "edgari@microservicios.com","5523473385"),
+( "javier godinez",  "Estado de Hidalgo numero 2", "javigodinez@microservicios.com","5451423785")
+;
+
+
+
+describe cuenta2;
+insert into cuenta2 (saldo, Cliente2_idCliente2) values 
+(10000.0, 1),
+(10000.0, 2),
+(10000.0, 3),
+(10000.0, 4),
+(10000.0, 5),
+(10000.0, 1),
+(10000.0, 2),
+(10000.0, 3),
+(10000.0, 4),
+(10000.0, 5)
+;
