e0d27316ee30750db88607d1739af1f0d33125d7
chore <luis.vazquez.ch08@gmail.com>
2018-01-24 13:51:28 -0600

    se agrego scrip de la base de datos

---
+++ demo2/databaseScript/Script.sql
@@ -0,0 +1,125 @@
+#//ip del servidor remoto mysql 172.22.25.116
+# donde ver la conversion de tipos de datos  de sql -  java https://alvinalexander.com/java/mysql-to-java-field-mappings-decimal-bigdecimal-scala 
+
+#1.-creacion de la base
+#2.- creacion de la tabla
+#3.- sp insertacliente
+#4.- sp insertaCuente
+#5.- sp TheProcedure
+
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Cliente2`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Cliente2` (
+  `id_cliente2` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `address` VARCHAR(45) NULL,
+  `email` VARCHAR(100) NULL,
+  `phone` VARCHAR(45) NULL,
+  PRIMARY KEY (`id_cliente2`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Cuenta2`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Cuenta2` (
+  `id_cuenta2` INT NOT NULL AUTO_INCREMENT,
+  `saldo` DECIMAL NULL,
+  `cliente2_id_cliente2` INT NOT NULL,
+  PRIMARY KEY (`id_cuenta2`, `cliente2_id_cliente2`),
+  INDEX `fk_cliente2_id_cliente2_idx` (`cliente2_id_cliente2` ASC),
+  CONSTRAINT `fk_cuenta2_cliente2`
+    FOREIGN KEY (`cliente2_id_cliente2`)
+    REFERENCES `mydb`.`Cliente2` (`id_cliente2`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
+
+USE `mydb`;
+DROP procedure IF EXISTS `insertarCliente`;
+DELIMITER $$
+USE `mydb`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insertarCliente`(
+  in name VARCHAR(45),
+  in address VARCHAR(45),
+  in email VARCHAR(100),
+  in phone VARCHAR(45) 
+)
+BEGIN
+	insert into cliente2 (name, address, email, phone) values
+		( name,address, email, phone);
+END $$
+DELIMITER ;
+
+
+
+USE `mydb`;
+DROP procedure IF EXISTS `insertarCuenta`;
+
+DELIMITER $$
+USE `mydb`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insertarCuenta`(
+ saldo DECIMAL,
+ cliente2_id_cliente2 INT
+)
+BEGIN
+  insert into cuenta2 (saldo, cliente2_id_cliente2) values
+  (`saldo`,`cliente2_id_cliente2`);
+END $$
+DELIMITER ;
+
+
+
+
+USE `mydb`;
+DROP procedure IF EXISTS `SPFinal1`;
+
+DELIMITER $$
+USE `mydb`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SPFinal1`(
+  in name VARCHAR(45),
+  in address VARCHAR(45),
+  in email VARCHAR(100),
+  in phone VARCHAR(45),
+  in saldo DECIMAL
+)
+BEGIN
+	declare idCliente int default 0;
+
+	select id_cliente2 into idCliente from cliente2 where (cliente2.name = name and cliente2.address = address and cliente2.email = email and cliente2.phone = phone) limit 1;
+
+	if idCliente = 0 then 
+		call insertarCliente(name, address, email, phone);
+	end if;
+    
+    select id_cliente2 into idCliente from cliente2 where (cliente2.name = name and cliente2.address = address and cliente2.email = email and cliente2.phone = phone) limit 1;
+
+	call insertarCuenta(saldo, idCliente);
+    
+END $$
+DELIMITER ;
+
