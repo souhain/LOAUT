093de8d2366f2526c69bc4a75a89ba022b7f920f
chore <luis.vazquez.ch08@gmail.com>
2018-01-24 13:36:43 -0600

    reset

--- BaseDeDatos/spconsultaSaldo.sql
+++
@@ -1,9 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `consultaSaldo`(
-	in idCliente int, 
-    in idCuenta int 
-)
-BEGIN
-	select Cliente2_idCliente2, idCuenta2, saldo   from cuenta2 
-    where Cliente2_idCliente2 = idCliente AND idCuenta2 = idCuenta;
-
-END
--- README.md
+++
@@ -1,1 +0,0 @@
-# clentes-cuenta2
