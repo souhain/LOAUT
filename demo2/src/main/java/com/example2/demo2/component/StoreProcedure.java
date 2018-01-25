7cfb99acec95467d545cbfba75502c28c81c9550
javarmgar <javargar@gmail.com>
2018-01-25 13:16:33 -0600

    Procedimiento almacenado sin usar ResultStorP

---
+++ demo2/src/main/java/com/example2/demo2/component/StoreProcedure.java
@@ -0,0 +1,101 @@
+package com.example2.demo2.component;
+
+import java.math.BigDecimal;
+import java.sql.*;
+
+import org.springframework.stereotype.Component;
+
+import com.example2.demo2.model.ResultStoreP;
+
+@Component("storeProcedure")
+public class StoreProcedure {
+
+	private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+	private static final String DB_CONNECTION = "jdbc:mysql://172.22.25.116:3306/mydb";
+	private static final String DB_USER = "root";
+	private static final String DB_PASSWORD = "root";
+	
+	private ResultStoreP resultStorep = null;
+	
+	
+	
+	
+	public void callStoredProcedure(String name, String address, String email, String phone, BigDecimal saldo ) throws SQLException {
+
+		Connection dbConnection = null;
+		CallableStatement callableStatement = null;
+
+		String SPFinal1 = "{call SPFinal1(?, ?, ?, ?, ?, ?, ?, ?)}";
+
+		try {
+			dbConnection = getDBConnection();
+			callableStatement = dbConnection.prepareCall(SPFinal1);
+
+			callableStatement.setString(1, name );
+			callableStatement.setString(2, address );
+			callableStatement.setString(3, email );
+			callableStatement.setString(4, phone );
+			callableStatement.setBigDecimal(5, saldo);
+			
+			
+			callableStatement.registerOutParameter(6, java.sql.Types.INTEGER);
+			callableStatement.registerOutParameter(7, java.sql.Types.INTEGER);
+			callableStatement.registerOutParameter(8, java.sql.Types.DECIMAL);
+
+			// execute SPFinal1 store procedure
+			callableStatement.executeUpdate();
+
+			int idClienteOut = callableStatement.getInt(6);
+			int idCuentaOut = callableStatement.getInt(7);
+			BigDecimal saldoOut = callableStatement.getBigDecimal(8);
+
+			System.out.println("idCliente: " + idClienteOut);
+			System.out.println("idCuenta : " + idCuentaOut);
+			System.out.println("saldo : " + saldoOut);
+
+		} catch (SQLException e) {
+
+			System.out.println(e.getMessage());
+
+		} finally {
+
+			if (callableStatement != null) {
+				callableStatement.close();
+			}
+
+			if (dbConnection != null) {
+				dbConnection.close();
+			}
+
+		}
+	}
+	
+	private Connection getDBConnection() {
+
+		Connection dbConnection = null;
+
+		try {
+
+			Class.forName(DB_DRIVER);
+
+		} catch (ClassNotFoundException e) {
+
+			System.out.println(e.getMessage());
+
+		}
+
+		try {
+
+			dbConnection = DriverManager.getConnection(
+				DB_CONNECTION, DB_USER,DB_PASSWORD);
+			return dbConnection;
+
+		} catch (SQLException e) {
+
+			System.out.println(e.getMessage());
+
+		}
+		return dbConnection;
+	}
+
+}
--- demo2/src/main/java/com/example2/demo2/service/impl/StoreProcedureServiceImpl.java
+++ demo2/src/main/java/com/example2/demo2/service/impl/StoreProcedureServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example2.demo2.service.impl;
 
 import java.math.BigDecimal;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
@@ -9,13 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import com.example2.demo2.component.StoreProcedure;
 import com.example2.demo2.controller.Cliente2Controller;
 import com.example2.demo2.model.Cliente2;
 import com.example2.demo2.model.Cuenta2;
 import com.example2.demo2.model.ResultStoreP;
 import com.example2.demo2.repository.Cliente2Repository;
 import com.example2.demo2.repository.Cuenta2Repository;
-import com.example2.demo2.repository.StoreProcedureRepository;
 import com.example2.demo2.service.StoreProcedureService;
 
 @Service("StoreProcedureServiceImpl")
@@ -25,31 +26,31 @@ public class StoreProcedureServiceImpl implements StoreProcedureService {
 	private static final Log LOGGER=LogFactory.getLog(StoreProcedureServiceImpl.class);
 
 	@Autowired
-	@Qualifier("StoreProcedureRepository")
-	private StoreProcedureRepository storeProcedureRepository;
+	@Qualifier("storeProcedure")
+	private StoreProcedure storeProcedure;
 	
 	
 
-	@Autowired
-	@Qualifier("cuenta2Repository")
-	private Cuenta2Repository cuenta2Repository;
 	
 	
 	@Override
 	public void insertarCliente(Cliente2 cliente2, Cuenta2 cuenta2) {
-		LOGGER.info("Method insertarCliente()" ) ; 
-		List<Cuenta2> c;
-		c= cuenta2Repository.SPFinal1(cliente2.getName(),
-				cliente2.getAddress(),
-				cliente2.getEmail(),
-				cliente2.getPhone(),
-				cuenta2.getSaldo()
-				);
+		LOGGER.info("Method insertarCliente()");
+		
+		try {
+			storeProcedure.callStoredProcedure(
+					cliente2.getName(),
+					cliente2.getAddress(),
+					cliente2.getEmail(),
+					cliente2.getPhone(),
+					cuenta2.getSaldo()
+					);
+		} catch (SQLException e) {
 
-		for(int i=0;i<c.size();i++) {
-			System.out.println(c.get(i));
+			e.printStackTrace();
 		}
-		System.out.println(c);
+
+
 	}
 
 }
