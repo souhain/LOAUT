df2641c2ecd18640f68f1e1700954b914e423f34
LuisChore <luis.vazquez.ch@hotmail.com>
2018-01-29 11:19:16 -0600

    first tests

--- demo2/src/main/java/com/example2/demo2/component/StoreProcedure.java
+++ demo2/src/main/java/com/example2/demo2/component/StoreProcedure.java
@@ -3,17 +3,18 @@ package com.example2.demo2.component;
 import java.math.BigDecimal;
 import java.sql.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import com.example2.demo2.model.ResultStoreP;
 
 @Component("storeProcedure")
 public class StoreProcedure {
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
 
-	private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
-	private static final String DB_CONNECTION = "jdbc:mysql://172.22.25.116:3306/mydb";
-	private static final String DB_USER = "root";
-	private static final String DB_PASSWORD = "root";
 	
 	
 	
@@ -26,7 +27,7 @@ public class StoreProcedure {
 		String SPFinal1 = "{call SPFinal1(?, ?, ?, ?, ?, ?, ?, ?)}";
 
 		try {
-			dbConnection = getDBConnection();
+			dbConnection =jdbcTemplate.getDataSource().getConnection();
 			callableStatement = dbConnection.prepareCall(SPFinal1);
 
 			callableStatement.setString(1, name );
@@ -71,32 +72,6 @@ public class StoreProcedure {
 		return resultStoreP; 
 	}
 	
-	private Connection getDBConnection() {
-
-		Connection dbConnection = null;
-
-		try {
-
-			Class.forName(DB_DRIVER);
-
-		} catch (ClassNotFoundException e) {
-
-			System.out.println(e.getMessage());
-
-		}
-
-		try {
-
-			dbConnection = DriverManager.getConnection(
-				DB_CONNECTION, DB_USER,DB_PASSWORD);
-			return dbConnection;
-
-		} catch (SQLException e) {
-
-			System.out.println(e.getMessage());
-
-		}
-		return dbConnection;
-	}
+	
 
 }
--- demo2/src/test/java/com/example2/demo2/Demo2ApplicationTests.java
+++ demo2/src/test/java/com/example2/demo2/Demo2ApplicationTests.java
@@ -1,16 +1,152 @@
 package com.example2.demo2;
 
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+import java.sql.SQLException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example2.demo2.component.StoreProcedure;
+import com.example2.demo2.model.Cliente2;
+import com.example2.demo2.model.Cuenta2;
+import com.example2.demo2.model.ResultStoreP;
+import com.example2.demo2.repository.Cliente2Repository;
+import com.example2.demo2.repository.Cuenta2Repository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Demo2ApplicationTests {
 
+	@Autowired
+	@Qualifier("storeProcedure")
+	StoreProcedure storeProcedure;
+
+	@Autowired
+	@Qualifier("cliente2Repository")
+	Cliente2Repository clienteRepository;
+
+	@Autowired
+	@Qualifier("cuenta2Repository")
+	Cuenta2Repository cuentaRepository;
+	
+	
+	
 	@Test
 	public void contextLoads() {
+	}
+	
+	
+	@Test
+	public void saldoTest() throws SQLException {
+		System.out.println("Test de agregar saldo");
+		ResultStoreP resultStoreP = storeProcedure.callStoredProcedure(
+				"Javie", 
+				"Emilio  25", 
+				"javarmgar@.com", 
+				"3", 
+				new BigDecimal( 70.0) );
+		
+		
+		cuentaRepository.delete(resultStoreP.getIdCuenta() );
+		clienteRepository.delete(resultStoreP.getIdCliente());
+
+		assertEquals("Error en el saldo", (new BigDecimal( 70.0 ) ), (resultStoreP.getSaldo() ) ) ;
+		
+	}
+	
+	
+	@Test
+	public void nameTest() throws SQLException {
+		System.out.println("Test nombre  ");
+		
+		ResultStoreP resultStoreP = storeProcedure.callStoredProcedure(
+				"Javi", 
+				"Emilio  25", 
+				"javarmgar@.com", 
+				"3", 
+				new BigDecimal( 70.0) );
+	
+		Cliente2 cl = clienteRepository.findOne(new Integer( resultStoreP.getIdCliente()));
+		Cuenta2 cu=cuentaRepository.findOne(resultStoreP.getIdCuenta());
+		
+		
+		cuentaRepository.delete(cu);
+		clienteRepository.delete(cl);
+
+		assertEquals("Error en el nombre", "Javier" , cl.getName());
+		
+		
+	}
+	
+	
+	@Test
+	public void addressTest() throws SQLException {
+		System.out.println("Test de address ");
+		
+		ResultStoreP resultStoreP = storeProcedure.callStoredProcedure(
+				"Javier", 
+				"Emilio  25", 
+				"javarmgar@.com", 
+				"3", 
+				new BigDecimal( 70.0) );
+	
+		Cliente2 cl = clienteRepository.findOne(new Integer( resultStoreP.getIdCliente()));
+		
+		cuentaRepository.delete(new Integer(resultStoreP.getIdCuenta() ) );
+		clienteRepository.delete(new Integer(resultStoreP.getIdCliente() ));
+		
+		assertEquals("Error en la direccion", "Emilio  25" , cl.getAddress());
+		
+	}
+	
+
+	@Test
+	public void emailTest() throws SQLException {
+		System.out.println("Test de email");
+		
+		ResultStoreP resultStoreP = storeProcedure.callStoredProcedure(
+				"Javier", 
+				"Emilio  25", 
+				"javarmgar@gmail.com", 
+				"3", 
+				new BigDecimal( 70.0) );
+	
+		Cliente2 cl = clienteRepository.findOne(new Integer( resultStoreP.getIdCliente()));
+		
+		
+		
+		cuentaRepository.delete(new Integer(resultStoreP.getIdCuenta() ) );
+		clienteRepository.delete(new Integer(resultStoreP.getIdCliente() ));
+		assertEquals("Error en el correo", "javarmgar@gmail.com" , cl.getAddress());
+	
+	}
+	
+	
+	@Test
+	public void phoneTest() throws SQLException {
+		System.out.println("Test de phone");
+		
+		ResultStoreP resultStoreP = storeProcedure.callStoredProcedure(
+				"Javier", 
+				"Emilio  25", 
+				"javarmgar@gmail.com", 
+				"3493284879", 
+				new BigDecimal( 70.0) );
+	
+		Cliente2 cl = clienteRepository.findOne(new Integer( resultStoreP.getIdCliente()));
+		
+		
+		
+		cuentaRepository.delete(new Integer(resultStoreP.getIdCuenta() ) );
+		clienteRepository.delete(new Integer(resultStoreP.getIdCliente() ));
+		assertEquals("Error en el telefono", "3493284879" , cl.getPhone());
+
 	}
 
 }
